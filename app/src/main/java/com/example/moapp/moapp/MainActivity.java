205b1e4ee9634d0da8ad3ce2887d3454cd084bf8
momomou <mou32123@hotmail.com>
2018-07-12 15:53:43 +0800

    Add: UI to show network information
        - When NetworkCallbacks are called, update UI
        - When click button, update UI

--- app/src/main/java/com/example/moapp/moapp/MainActivity.java
+++ app/src/main/java/com/example/moapp/moapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.moapp.moapp;
 
+import android.net.LinkAddress;
+import android.net.RouteInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+import android.text.method.ScrollingMovementMethod;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -16,16 +23,28 @@ import android.net.NetworkCapabilities;
 import android.net.LinkProperties;
 import android.net.Network;
 
+import android.os.HandlerThread;
+import android.os.Message;
+
+import java.net.InetAddress;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "yumike";
     private static final Boolean LOG_ON_UI = false;
 
     private TextView mTextMessage;
+    private Button mBtn;
 
     private Context mCtx;
     private static ConnectivityManager mCM;
     private static ConnectivityManager.NetworkCallback mNetworkCallback;
     private static ConnectivityManager.NetworkCallback mNetworkCallback2;
+    private HandlerThread mHandlerThread;
+    private Handler mHandler;
+
+    private Handler mMainHandler;
+
+    private int cnt = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,16 +71,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         myLog(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        myLog(TAG, "onResume");
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage = (TextView) findViewById(R.id.txv1);
+        mTextMessage.setMovementMethod(new ScrollingMovementMethod());
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mBtn = (Button) findViewById(R.id.btn1);
+        mBtn.setOnClickListener(onClickToGetNetwork);
 
         mCM = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -69,6 +87,42 @@ public class MainActivity extends AppCompatActivity {
         NetworkRequest.Builder builder = new NetworkRequest.Builder();
         NetworkRequest request = builder.build();
         mCM.registerNetworkCallback(request, mNetworkCallback);
+
+        mHandlerThread = new HandlerThread("myHandlerThread");
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper()) {
+            public void handleMessage(Message msg) {
+                String str = (String) msg.obj;
+                myLog(TAG, "msg.obj " + str);
+                ShowNetworkInformation();
+                mHandler.sendEmptyMessageDelayed(1, 3000);
+                Message msg2 = Message.obtain(msg) ;
+                //msg2.setTarget(mMainHandler);
+                msg2.obj = String.valueOf(cnt++);
+                mMainHandler.sendMessage(msg2);
+            }
+        };
+
+        Message msg = Message.obtain(mHandler);
+        msg.obj = String.valueOf(cnt);
+        //msg.sendToTarget();
+
+        mMainHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                String str = (String) msg.obj;
+                myLog(TAG, "setText " + str);
+                mTextMessage.setText(str);
+            }
+        };
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myLog(TAG, "onResume");
+
     }
 
     @Override
@@ -92,8 +146,57 @@ public class MainActivity extends AppCompatActivity {
         Network[] networks = mCM.getAllNetworks();
         for (Network nw : networks) {
             NetworkInfo ni = mCM.getNetworkInfo(nw);
-            //myLog(TAG, ni.toString());
+            LinkProperties lp = mCM.getLinkProperties(nw);
+            NetworkCapabilities nc = mCM.getNetworkCapabilities(nw);
+            myLog(TAG, nw != null? nw.toString() : "null");
+            myLog(TAG, ni != null? ni.toString() : "null");
+            myLog(TAG, lp != null? lp.toString() : "null");
+            myLog(TAG, nc != null? nc.toString() : "null");
         }
+        updateUI();
+    }
+
+    public void updateUI() {
+        Network[] networks = mCM.getAllNetworks();
+        StringBuilder sb = new StringBuilder();
+
+        for (Network nw : networks) {
+            NetworkInfo ni = mCM.getNetworkInfo(nw);
+            LinkProperties lp = mCM.getLinkProperties(nw);
+            NetworkCapabilities nc = mCM.getNetworkCapabilities(nw);
+
+            sb.append("Network ").append(nw.toString()).append("\n");
+
+            StringBuilder networkInfo = sb.append("NetworkInfo").append("\n").
+                    append("  DetailedState: ").append(ni.getDetailedState()).append("\n");
+
+            /*
+            sb.append("NetworkCapabilities").append("\n").
+                    append(nc.toString()).append("\n");
+            */
+
+            String linkAddresses = "";
+            for (LinkAddress addr : lp.getLinkAddresses()) linkAddresses += "  LinkAddresses: " + addr.toString() + "\n";
+
+            String routes = "";
+            for (RouteInfo route : lp.getRoutes()) routes += "  Routes: " + route.toString() + "\n";
+
+            String dns = "";
+            for (InetAddress addr : lp.getDnsServers()) dns += "  DnsServers: " + addr.toString() + "\n";
+
+
+            sb.append("LinkProperties").append("\n").
+                    append("  Interface: ").append(lp.getInterfaceName()).append("\n").
+                    append(linkAddresses).
+                    append(routes).
+                    append(dns);
+
+            sb.append("\n");
+        }
+
+        Message msg = Message.obtain(mMainHandler);
+        msg.obj = sb.toString();
+        msg.sendToTarget();
     }
 
     private class NetworkCallbackImpl extends ConnectivityManager.NetworkCallback {
@@ -125,4 +228,13 @@ public class MainActivity extends AppCompatActivity {
             ShowNetworkInformation();
         }
     }
+
+    private View.OnClickListener onClickToGetNetwork = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ShowNetworkInformation();
+            //updateUI();
+        }
+    };
+
 }
